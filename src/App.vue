b400f3d4134894cf8586ffdeb1b27a9dd76ec9e9
Mark van Wyk <mark@flyh2.com>
2021-04-30 21:42:58 +0200

    Vite + TailwindCSS working nicely

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="flex flex-col items-center w-full">
+        <img alt="Vue logo" src="./assets/logo.png" class="w-1/5" />
+        <HelloWorld msg="Hello Vue 3 + Vite" />
+    </section>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1 class="text-2xl">{{ msg }}</h1>
+    <p class="mt-2">
+        <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
+        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    </p>
+    <button @click="state.count++" class="bg-green-500 my-2 p-1 rounded-md border-2 shadow-sm shadow text-white">count is: {{ state.count }}</button>
+    <p>
+        Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+</template>
+
+<script setup>
+    import {defineProps, reactive} from 'vue'
+
+    defineProps({
+        msg: String
+    })
+
+    const state = reactive({count: 0})
+</script>
+
+<style scoped>
+    a {
+        color: #42b983;
+    }
+</style>
