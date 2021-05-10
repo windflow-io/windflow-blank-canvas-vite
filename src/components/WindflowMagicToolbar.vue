97c11add08050c430309e4e8c88ac276f8d185fd
Mark van Wyk <mark@flyh2.com>
2021-05-10 22:21:36 +0200

    Toolbar complete

--- src/components/WindflowMagicToolbar.vue
+++ src/components/WindflowMagicToolbar.vue
@@ -1,31 +1,90 @@
 <template>
-    <div class="absolute bg-gray-100 rounded-md top-0 left-0 top-0 ml-10 mt-10 bg-white shadow-md flex flex-row items-center justify-center text-gray-600 text-lg py-3">
-        <div class="pl-3 pr-2.5 py-1 border-r border-gray-300 flex cursor-move"><grip-vertical-icon/></div>
-        <button class="py-1 px-2.5 border-r border-gray-300 flex flex-col"><plus-icon/></button>
-        <button class="py-1 px-2.5 border-r border-gray-300 flex"><edit-icon/></button>
-        <button class="py-1 px-2.5 border-r border-gray-300 flex"><code-icon/></button>
-        <button class="py-1 px-2.5 border-r border-gray-300 flex"><palette-icon/></button>
-        <button class="py-1 px-2.5 border-r border-gray-300 flex"><trash-icon/></button>
-        <button class="py-1 px-2.5 border-r border-gray-300 flex"><save-icon/></button>
-        <button class="py-1 px-2.5 border-r border-gray-300 flex"><upload-icon/></button>
-        <button class="py-1 px-2.5 flex border-r border-gray-300"><cog-icon/></button>
-        <button class="py-1 px-2.5 pr-3 flex"><times-icon/></button>
+    <div ref="windflowMagicToolbar" class="absolute bg-gray-100 rounded-md shadow-md flex flex-col items-center justify-center text-gray-600 text-lg py-2" :style="{left:positionX + 'px', top:positionY + 'px'}">
+        <div class="flex flex-row">
+
+            <windflow-button class="cursor-move" @mousedown="dragDown($event)">
+                <grip-vertical-icon/>
+            </windflow-button>
+
+            <div class="w-0 border-r h-5 mt-2 border-gray-500"></div>
+
+            <windflow-button @mouseover="statusbarText = 'Add component'" @mouseout="statusbarText = ''">
+                <plus-icon/>
+            </windflow-button>
+
+            <windflow-button @mouseover="statusbarText = 'Edit content'" @mouseout="statusbarText = ''">
+                <edit-icon/>
+            </windflow-button>
+
+            <windflow-button @mouseover="statusbarText = 'Edit code'" @mouseout="statusbarText = ''" selected="bg-blue-200 hover:bg-blue-300">
+                <code-icon/>
+            </windflow-button>
+
+            <windflow-button @mouseover="statusbarText = 'Remove component'" @mouseout="statusbarText = ''">
+                <trash-icon/>
+            </windflow-button>
+
+            <windflow-button @mouseover="statusbarText = 'Save all'" @mouseout="statusbarText = ''">
+                <save-icon/>
+            </windflow-button>
+
+            <windflow-button @mouseover="statusbarText = 'Publish all'" @mouseout="statusbarText = ''">
+                <upload-icon/>
+            </windflow-button>
+
+            <windflow-button @mouseover="statusbarText = 'Close'" @mouseout="statusbarText = ''">
+                <times-icon/>
+            </windflow-button>
+        </div>
+        <div class="flex pl-2 w-full items-start text-xs items-center transform ease-linear duration-200" :class="statusbarText ? 'pt-1 h-4':'h-0'"><span>{{statusbarText}} <!--<edit-icon/>--></span></div>
     </div>
 </template>
 
-<script setup>
+<script>
 
-import GripVerticalIcon from "/src/components/icons/GripVerticalIcon.vue";
-import PlusIcon from "/src/components/icons/PlusIcon.vue"
-import EditIcon from "/src/components/icons/EditIcon.vue"
-import CodeIcon from "/src/components/icons/CodeIcon.vue"
-import PaletteIcon from "/src/components/icons/PaletteIcon.vue"
-import TrashIcon from "/src/components/icons/TrashIcon.vue"
-import TimesIcon from "/src/components/icons/TimesIcon.vue"
-import UploadIcon from "/src/components/icons/UploadIcon.vue"
-import SaveIcon from "/src/components/icons/SaveIcon.vue"
-import CogIcon from "/src/components/icons/CogIcon.vue"
+import PlusIcon from '/src/components/core/icons/PlusIcon.vue'
+import CodeIcon from '/src/components/core/icons/CodeIcon.vue'
+import EditIcon from '/src/components/core/icons/EditIcon.vue'
+import SaveIcon from '/src/components/core/icons/SaveIcon.vue'
+import TrashIcon from '/src/components/core/icons/TrashIcon.vue'
+import UploadIcon from '/src/components/core/icons/UploadIcon.vue'
+import TimesIcon from '/src/components/core/icons/TimesIcon.vue'
+import WindflowButton from '/src/components/core/WindflowButton.vue'
+import GripVerticalIcon from "/src/components/core/icons/GripVerticalIcon.vue";
 
+
+export default {
+    name: 'WindflowMagicToolbar',
+    components: {
+        GripVerticalIcon,
+        CodeIcon, PlusIcon, EditIcon, SaveIcon, TrashIcon, UploadIcon, TimesIcon, WindflowButton
+    },
+    data() {
+        return {
+            statusbarText: '',
+            positionX:'200',
+            positionY:'200',
+            mouseOffsetX:0,
+            mouseOffsetY:0,
+        }
+    },
+    methods: {
+        dragDown(e) {
+            this.mouseOffsetX = e.clientX - this.$refs.windflowMagicToolbar.getBoundingClientRect().x;
+            this.mouseOffsetY = e.clientY - this.$refs.windflowMagicToolbar.getBoundingClientRect().y;
+            window.addEventListener('mousemove', this.dragMove)
+            window.addEventListener('mouseup',this.dragUp)
+        },
+        dragMove(e) {
+            this.positionX = e.clientX - this.mouseOffsetX;
+            this.positionY = e.clientY - this.mouseOffsetY;
+        },
+        dragUp() {
+            window.removeEventListener('mousemove', this.dragMove);
+            window.removeEventListener('mouseup', this.dragUp);
+        }
+    }
+}
 
 
 </script>
---
+++ src/components/core/WindflowButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="relative flex flex-col items-center justify-center py-2 w-10 rounded" :class="selected || 'hover:bg-gray-200'">
+        <button class="flex flex-col" v-if="action"><slot/></button>
+        <div class="flex flex-col" v-else><slot/></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WindflowButton",
+    props: [
+        'tooltip',
+        'action',
+        'selected'
+    ]
+}
+</script>
