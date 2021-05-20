f20b6e93c5e0b1935d4795fcf1d7d0ddce403752
Mark van Wyk <mark@flyh2.com>
2021-05-20 14:50:24 +0200

    importing correctly

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
-    <windflow-layout></windflow-layout>
-    <windflow-magic-toolbar></windflow-magic-toolbar>
-
-    <animation-one></animation-one>
-
+    Toolbar
+    <windflow-magic-toolbar/>
 </template>
 
 <script lang="ts">
 
     import {WindflowMagicToolbar} from '@windflow/windflow-magic-toolbar'
-    import WindflowLayout from '@/components/WindflowLayout.vue';
-    import AnimationOne from '@/components/AnimationOne.vue';
 
     export default {
         name: 'App',
-        components: {WindflowLayout, WindflowMagicToolbar, AnimationOne}
+        components: {WindflowMagicToolbar}
     }
 
 </script>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [vue()],
+    external: ['vue'],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, '/src'),
